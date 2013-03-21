7b750c63498709e61e610c641003825a89b1549d
Sebastian Jeworutzki <Sebastian.Jeworutzki@ruhr-uni-bochum.de>
2013-03-21 17:16:54 +0100

    constructor to make $REX vars usable in class

--- plugins/auth_simplesamlphp/classes/class.rex_com_auth_simplesamlphp.inc.php
+++ plugins/auth_simplesamlphp/classes/class.rex_com_auth_simplesamlphp.inc.php
@@ -2,6 +2,19 @@
 
 class rex_com_auth_simplesamlphp
 {
+
+    public function __construct() {
+        global $REX;
+        $this->authSource = $REX['ADDON']['community']['plugin_auth_simplesamlphp']['authSource'];
+        $this->simplesamlphpPath = $REX['ADDON']['community']['plugin_auth_simplesamlphp']['simplesamlphpPath'];
+        $this->hash_func = $REX['ADDON']['community']['plugin_auth']['passwd_algorithmus'];            
+        $this->isHashed = $REX['ADDON']['community']['plugin_auth']['passwd_hashed'];
+        $this->article_login_failed = $REX['ADDON']['community']['plugin_auth']['article_login_failed'];
+        $this->article_login_ok = $REX['ADDON']['community']['plugin_auth']['article_login_ok'];
+        $this->redirect = $REX['ADDON']['community']['plugin_auth_simplesamlphp']['redirect'];
+        $this->defaultgroups = $REX['ADDON']['community']['plugin_auth_simplesamlphp']['defaultgroups'];
+        $this->synctranslation = $REX['ADDON']['community']['plugin_auth_simplesamlphp']['synctranslation'];
+    }
 	//
 	// Generates a simple random password
 	//
@@ -18,11 +31,12 @@ class rex_com_auth_simplesamlphp
     // Authentificate user with simplesamlphp
     // creates local user
     // 
-    public function authSaml($authSource = 'default-sp', $simplesamlphpPath ) 
+    public function authSaml() 
     {
+        print($this->simplesamlphpPath);
         // load simplesamlph and require authentification
-        require_once($simplesamlphpPath);
-        $as = new SimpleSAML_Auth_Simple($authSource);
+        require_once($this->simplesamlphpPath);
+        $as = new SimpleSAML_Auth_Simple($this->authSource);
         $as->requireAuth();
         $attributes = $as->getAttributes();
 
@@ -47,24 +61,23 @@ class rex_com_auth_simplesamlphp
                 $iu->setValue("login",$eduPersonTargetedID);
                 
                 // hash passwort if password are encrypted
-                $hash_func = $REX['ADDON']['community']['plugin_auth']['passwd_algorithmus'];
                 $password = rex_com_auth_simplesamlphp::generatePassword('16');
-                if($REX['ADDON']['community']['plugin_auth']['passwd_hashed']) 
+                if($this->isHashed) 
                 {
-                    $password = hash($hash_func, $password);
+                    $password = hash($this->hash_func, $password);
                 }
                 $iu->setValue("password", $password);
 
                 // Adding defaultgroups
-                if(isset($REX['ADDON']['community']['plugin_auth_simplesamlphp']['defaultgroups'])) 
+                if(isset($this->defaultgroups)) 
                 {
-                    $iu->setValue("rex_com_group",implode(',' , $REX['ADDON']['community']['plugin_auth_simplesamlphp']['defaultgroups']));
+                    $iu->setValue("rex_com_group",implode(',' , $this->defaultgroups));
                 }
 
                 // Translate datafields
-                if(!empty($REX['ADDON']['community']['plugin_auth_simplesamlphp']['synctranslation']))
+                if(!empty($this->synctranslation))
                 {
-                    foreach($REX['ADDON']['community']['plugin_auth_simplesamlphp']['synctranslation'] as $key => $value)
+                    foreach($this->synctranslation as $key => $value)
                     {
                         $iu->setValue($key,$attributes[$value]);
                     }
@@ -85,16 +98,16 @@ class rex_com_auth_simplesamlphp
             print_r($params);
             rex_com_auth::loginWithParams($params);
 
-            if(rex_com_auth::getUser() && $REX['ADDON']['community']['plugin_auth_simplesamlphp']['redirect'])
+            if(rex_com_auth::getUser() && $this->redirect)
             {
-                rex_redirect($REX['ADDON']['community']['plugin_auth']['article_login_ok']);
+                rex_redirect($this->article_login_ok);
             }
 
         } else 
         { // ------------------------- Login Error
-            if(!(rex_com_auth::getUser()) && $REX['ADDON']['community']['plugin_auth_simplesamlphp']['redirect']) 
+            if(!(rex_com_auth::getUser()) && $this->redirect) 
             {
-                rex_redirect($REX['ADDON']['community']['plugin_auth']['article_login_failed'],'',array('rex_com_auth_info'=>'2'));
+                rex_redirect($this->article_login_failed,'',array('rex_com_auth_info'=>'2'));
             }
         }
     }
